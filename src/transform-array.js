1dbd7a0e58f78ff0523da2bded879a11c09635ee
veronsmole <veronsmole@gmail.com>
2021-09-19 23:40:21 +0300

    feat: add code to transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,19 +1,58 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+    let newArr = [];
+    let skipNext = false;
+    for (let i = 0; i < arr.length; i++) {
+        if (!Array.isArray(arr)) {
+            throw new Error("'arr' parameter must be an instance of the Array!");
+        }
+        if (skipNext) {
+            skipNext = false;
+            continue;
+        }
+
+        switch (arr[i]) {
+            case "--discard-next":
+                if (arr[i + 1] !== undefined) {
+                    skipNext = true;
+                    continue;
+                } else continue;
+
+            case "--discard-prev" :
+                if (arr[i - 1] !== undefined) {
+                    newArr.pop()
+                } else continue;
+                break;
+
+            case "--double-next" :
+                if (arr[i + 1] !== undefined) {
+                    newArr.push(arr[i + 1]);
+                } else continue;
+                break;
+
+            case "--double-prev" :
+                if (arr[i - 1] !== undefined) {
+                    newArr.push(arr[i - 1]);
+                } else continue;
+                break;
+
+            default:
+                newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 }
